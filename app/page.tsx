9d87d4a78a7db5b66302f73f0b2061e54272e846
cliveskinner <clive.skinner@marketcast.com>
2024-04-14 01:02:33 +0100

    Added Cognito auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+// import { useForm } from 'react-hook-form';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
