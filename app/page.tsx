e9560e9fa19b80b09690798eb2f194390dbe76e7
cliveskinner <clive.skinner@marketcast.com>
2024-04-14 21:16:31 +0100

    Add identity provider

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from '../app/aws-exports';
+import awsExports from '../src/aws-exports';
 Amplify.configure(awsExports);
 
 import { Analytics } from "@vercel/analytics/react";
