86a0b81158370af28028fcaa9482c762099fa5bd
cliveskinner <clive.skinner@marketcast.com>
2024-04-15 21:25:36 +0100

    Add identity provider

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default async function App() {
     const openSaml = () => {
         const url: string = 'https://chatmc.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=i72c9bfndnimh63o0nuoalvrq&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fchatgpt-dev.marketcast.ninja%2F';
         if (typeof window !== "undefined") {
-            window.location.push(url);
+            window.location.href(url);
         }
         return(<div />)
     };
