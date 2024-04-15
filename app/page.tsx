64ceb37e50ffce45e37ea79cfcd98a9f4dcb4c3f
cliveskinner <clive.skinner@marketcast.com>
2024-04-15 17:21:12 +0100

    Add identity provider

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,83 @@
 import { Amplify } from 'aws-amplify';
 import { signInWithRedirect, getCurrentUser, AuthUser } from 'aws-amplify/auth';
 import { Hub } from "aws-amplify/utils";
-import awsExports from '../src/aws-exports';
-Amplify.configure(awsExports);
+import awsExports from '../app/aws-exports';
+
+import {
+  fetchAuthSession,
+  CredentialsAndIdentityIdProvider,
+  CredentialsAndIdentityId,
+  GetCredentialsOptions,
+  AuthTokens,
+} from 'aws-amplify/auth';
+import { CognitoIdentity } from '@aws-sdk/client-cognito-identity';
+const cognitoidentity = new CognitoIdentity({
+  region: awsExports.aws_project_region,
+});
+
+// Note: The custom provider class must implement CredentialsAndIdentityIdProvider
+class CustomCredentialsProvider implements CredentialsAndIdentityIdProvider {
+
+  // Example class member that holds the login information
+  federatedLogin?: {
+    domain: string,
+    token: string
+  };
+
+  // Custom method to load the federated login information
+  loadFederatedLogin(login?: typeof this.federatedLogin) {
+    // You may also persist this by caching if needed
+    this.federatedLogin = login;
+  }
+
+  async getCredentialsAndIdentityId(
+    getCredentialsOptions: GetCredentialsOptions
+  ): Promise<CredentialsAndIdentityId | undefined> {
+    try {
+
+      // You can add in some validation to check if the token is available before proceeding
+      // You can also refresh the token if it's expired before proceeding
+
+      const getIdResult = await cognitoidentity.getId({
+        // Get the identityPoolId from config
+        IdentityPoolId: awsExports.aws_cognito_identity_pool_id,
+        Logins: { [this.federatedLogin!.domain]: this.federatedLogin!.token },
+      });
+
+      const cognitoCredentialsResult = await cognitoidentity.getCredentialsForIdentity({
+        IdentityId: getIdResult.IdentityId,
+        Logins: { [this.federatedLogin!.domain]: this.federatedLogin!.token },
+      });
+
+      const credentials: CredentialsAndIdentityId = {
+        credentials: {
+          accessKeyId: cognitoCredentialsResult.Credentials?.AccessKeyId!,
+          secretAccessKey: cognitoCredentialsResult.Credentials?.SecretKey!,
+          sessionToken: cognitoCredentialsResult.Credentials?.SessionToken!,
+          expiration: cognitoCredentialsResult.Credentials?.Expiration!,
+        },
+        identityId: getIdResult.IdentityId,
+      };
+      return credentials;
+    } catch (e) {
+      console.log('Error getting credentials: ', e);
+    }
+  }
+  // Implement this to clear any cached credentials and identityId. This can be called when signing out of the federation service.
+  clearCredentialsAndIdentityId(): void {}
+}
+
+// Create an instance of your custom provider
+const customCredentialsProvider = new CustomCredentialsProvider();
+Amplify.configure(awsExports, {
+  Auth: {
+    // Supply the custom credentials provider to Amplify
+    credentialsProvider: customCredentialsProvider
+  },
+});
 
 import { Home } from "./components/home";
-
 import { getServerSideConfig } from "./config/server";
-
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
