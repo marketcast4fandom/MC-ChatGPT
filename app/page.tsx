fd196cfd8ac0661abd690bd5a9cb022d82e5cded
cliveskinner <clive.skinner@marketcast.com>
2024-04-16 23:15:32 +0100

    Add identity provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,10 @@
 'use client';
 
-import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import ConfigureAmplifyClientSide from './ConfigureAmplify';
-
-// export default async function App({ signOut, user } : WithAuthenticatorProps) {
-// // export function App({ signOut, user } : WithAuthenticatorProps) {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <ConfigureAmplifyClientSide />
-//       <h1>Hello {user?.username}</h1>
-//       <button onClick={signOut}>Sign out</button>
-//     </main>
-//   );
-// }
-
-// 'use client'
-// import { Amplify } from 'aws-amplify';
-import { signInWithRedirect, getCurrentUser, AuthUser } from 'aws-amplify/auth';
+import { signInWithRedirect, AuthError } from 'aws-amplify/auth';
 import { Hub } from "aws-amplify/utils";
 import { Home } from "./components/home";
-// import { getServerSideConfig } from "./config/server";
-// const serverConfig = getServerSideConfig();
 
 export default async function App() {
 
@@ -55,12 +37,18 @@ export default async function App() {
       }
     });
 
-    function handleSignIn() {
-        signInWithRedirect({
-            provider: {
-                custom: "MarketCastOkta"
-            },
-        });
+    async function handleSignIn() {
+        try {
+            await signInWithRedirect({
+                provider: {
+                    custom: "MarketCastOkta"
+                },
+            });
+        } catch (error) {
+            error instanceof AuthError && console.log(error.name, error.message, error.recoverySuggestion)
+            // if (error.name === 'UserAlreadyAuthenticatedException') {
+            // }
+        }
         return(<></>)
     }
 
