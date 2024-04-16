87f39089ca36982b776ee2ba80244ae6b60e0b25
cliveskinner <clive.skinner@marketcast.com>
2024-04-16 12:26:37 +0100

    Add identity provider

---
+++ app/ConfigureAmplify.tsx
@@ -0,0 +1,9 @@
+import { Amplify } from "aws-amplify";
+import awsExports from '../src/aws-exports';
+Amplify.configure(awsExports, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+  return null;
+}
+
+Amplify.configure(awsExports);
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,53 @@
-'use client'
-import { Amplify } from 'aws-amplify';
+'use client';
+
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import ConfigureAmplifyClientSide from './ConfigureAmplify';
+
+// export default async function App({ signOut, user } : WithAuthenticatorProps) {
+// // export function App({ signOut, user } : WithAuthenticatorProps) {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <ConfigureAmplifyClientSide />
+//       <h1>Hello {user?.username}</h1>
+//       <button onClick={signOut}>Sign out</button>
+//     </main>
+//   );
+// }
+
+// 'use client'
+// import { Amplify } from 'aws-amplify';
 import { signInWithRedirect, getCurrentUser, AuthUser } from 'aws-amplify/auth';
-import { Hub } from "aws-amplify/utils";
+// import { Hub } from "aws-amplify/utils";
 import awsExports from '../src/aws-exports';
-
+//
 import { Home } from "./components/home";
-import { getServerSideConfig } from "./config/server";
-const serverConfig = getServerSideConfig();
-
+// import { getServerSideConfig } from "./config/server";
+// const serverConfig = getServerSideConfig();
+//
 export default async function App() {
 
-    Hub.listen("auth", ({ payload }) => {
-        switch (payload.event) {
-            case "signInWithRedirect":
-                const user = getCurrentUser();
-                console.log(user);
-                return(
-                    <><Home /></>
-                )
-                break;
-            case "signInWithRedirect_failure":
-                // handle sign in failure
-                console.log(payload.event);
-                break;
-            case "customOAuthState":
-                const state = payload.data; // this will be customState provided on signInWithRedirect function
-                console.log(state);
-                break;
-      }
-    });
-
+//     Hub.listen("auth", ({ payload }) => {
+//         switch (payload.event) {
+//             case "signInWithRedirect":
+//                 const user = getCurrentUser();
+//                 console.log(user);
+//                 return(
+//                     <><Home /></>
+//                 )
+//                 break;
+//             case "signInWithRedirect_failure":
+//                 // handle sign in failure
+//                 console.log(payload.event);
+//                 break;
+//             case "customOAuthState":
+//                 const state = payload.data; // this will be customState provided on signInWithRedirect function
+//                 console.log(state);
+//                 break;
+//       }
+//     });
+//
     const provider = {
       custom: 'MarketCastOkta'
     }
@@ -37,22 +55,14 @@ export default async function App() {
     function handleSignInClick() {
         console.log("pre signInWithRedirect")
         console.log(awsExports)
-        signInWithRedirect();
+        signInWithRedirect({provider: provider});
         console.log("post signInWithRedirect")
         return(<><Home /></>)
     }
 
-//     const openSaml = () => {
-// //         const url: string = 'https://chatmc.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=i72c9bfndnimh63o0nuoalvrq&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fchatgpt-dev.marketcast.ninja%2F';
-// //         if (typeof window !== "undefined") {
-// //             window.open("https://chatmc.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=i72c9bfndnimh63o0nuoalvrq&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fchatgpt-dev.marketcast.ninja%2F", "_blank");
-// //         }
-//         window.open("https://chatmc.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=i72c9bfndnimh63o0nuoalvrq&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fchatgpt-dev.marketcast.ninja%2F", "_blank");
-//         return(<div />)
-//     };
-
     return (
         <div>
+            <ConfigureAmplifyClientSide />
             { handleSignInClick() }
         </div>
     );
