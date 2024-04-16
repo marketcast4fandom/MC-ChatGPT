44897c58b504e00ab843a53b27ca8f80fef1362e
cliveskinner <clive.skinner@marketcast.com>
2024-04-16 14:55:45 +0100

    Add identity provider

--- app/page.tsx
+++ app/page.tsx
@@ -19,52 +19,54 @@ import ConfigureAmplifyClientSide from './ConfigureAmplify';
 // 'use client'
 // import { Amplify } from 'aws-amplify';
 import { signInWithRedirect, getCurrentUser, AuthUser } from 'aws-amplify/auth';
-// import { Hub } from "aws-amplify/utils";
-import awsExports from '../src/aws-exports';
-//
+import { Hub } from "aws-amplify/utils";
 import { Home } from "./components/home";
 // import { getServerSideConfig } from "./config/server";
 // const serverConfig = getServerSideConfig();
 //
 export default async function App() {
 
-//     Hub.listen("auth", ({ payload }) => {
-//         switch (payload.event) {
-//             case "signInWithRedirect":
-//                 const user = getCurrentUser();
-//                 console.log(user);
-//                 return(
-//                     <><Home /></>
-//                 )
-//                 break;
-//             case "signInWithRedirect_failure":
-//                 // handle sign in failure
-//                 console.log(payload.event);
-//                 break;
-//             case "customOAuthState":
-//                 const state = payload.data; // this will be customState provided on signInWithRedirect function
-//                 console.log(state);
-//                 break;
-//       }
-//     });
-//
-    const provider = {
-      custom: 'MarketCastOkta'
-    }
+    Hub.listen("auth", ({ payload }) => {
+        switch (payload.event) {
+            case "signInWithRedirect":
+                console.log("signInWithRedirect");
+                return(
+                    <><Home /></>
+                )
+            case "signInWithRedirect_failure":
+                // handle sign in failure
+                console.log(payload.event);
+                break;
+            case "customOAuthState":
+                const state = payload.data; // this will be customState provided on signInWithRedirect function
+                console.log(state);
+                break;
+      }
+    });
 
-    function handleSignInClick() {
-        console.log("pre signInWithRedirect")
-        console.log(awsExports)
-        signInWithRedirect({provider: provider});
-        console.log("post signInWithRedirect")
-        return(<><Home /></>)
+
+    function handleSignIn() {
+        signInWithRedirect({
+            provider: {
+                custom: "MarketCastOkta"
+            },
+            options: {
+                preferPrivateSession: true,
+            },
+        });
+        return(<></>)
     }
 
     return (
-        <div>
-            <ConfigureAmplifyClientSide />
-            { handleSignInClick() }
-        </div>
+        // <>
+        //     <ConfigureAmplifyClientSide />
+        //     { handleSignIn() }
+        // </>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <ConfigureAmplifyClientSide />
+          <button onClick={handleSignIn}>Sign in with Okta</button>
+        </main>
+
     );
 }
 
