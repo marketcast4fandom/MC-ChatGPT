556803f44b0b98857462254a142bad0c63138a1f
cliveskinner <clive.skinner@marketcast.com>
2024-04-17 11:38:48 +0100

    Add identity provider

--- app/page.tsx
+++ app/page.tsx
@@ -16,39 +16,39 @@ export default async function App() {
     }
     const [page, setPage] = useState(error_page);
 
-    useEffect(() => {
-        Hub.listen('auth', ({ payload }) => {
-            switch (payload.event) {
-                case 'signedIn':
-                    console.log('user have been signedIn successfully.');
-                    setPage(home_page)
-                    break;
-                case 'signedOut':
-                    console.log('user have been signedOut successfully.');
-                    setPage(error_page)
-                    break;
-                case 'tokenRefresh':
-                    console.log('auth tokens have been refreshed.');
-                    setPage(home_page)
-                    break;
-                case 'tokenRefresh_failure':
-                    console.log('failure while refreshing auth tokens.');
-                    setPage(error_page)
-                    break;
-                case 'signInWithRedirect':
-                    console.log('signInWithRedirect API has successfully been resolved.');
-                    setPage(home_page)
-                    break;
-                case 'signInWithRedirect_failure':
-                    console.log('failure while trying to resolve signInWithRedirect API.');
-                    setPage(error_page)
-                    break;
-                case 'customOAuthState':
-                    console.log('custom state returned from CognitoHosted UI');
-                    break;
-            }
-        });
-    }, []);
+    // useEffect(() => {
+    //     Hub.listen('auth', ({ payload }) => {
+    //         switch (payload.event) {
+    //             case 'signedIn':
+    //                 console.log('user have been signedIn successfully.');
+    //                 setPage(home_page)
+    //                 break;
+    //             case 'signedOut':
+    //                 console.log('user have been signedOut successfully.');
+    //                 setPage(error_page)
+    //                 break;
+    //             case 'tokenRefresh':
+    //                 console.log('auth tokens have been refreshed.');
+    //                 setPage(home_page)
+    //                 break;
+    //             case 'tokenRefresh_failure':
+    //                 console.log('failure while refreshing auth tokens.');
+    //                 setPage(error_page)
+    //                 break;
+    //             case 'signInWithRedirect':
+    //                 console.log('signInWithRedirect API has successfully been resolved.');
+    //                 setPage(home_page)
+    //                 break;
+    //             case 'signInWithRedirect_failure':
+    //                 console.log('failure while trying to resolve signInWithRedirect API.');
+    //                 setPage(error_page)
+    //                 break;
+    //             case 'customOAuthState':
+    //                 console.log('custom state returned from CognitoHosted UI');
+    //                 break;
+    //         }
+    //     });
+    // }, []);
 
     async function handleSignIn() {
         // try {
